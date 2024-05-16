f2c98e0112ed1bdeaa00c4a4b26b4a1e0bcd2972
Irwanto Wijaya <it.irwanto@rsmurniteguh.com>
2024-05-16 13:06:18 +0700

    cp

--- Team-7/script.js
+++ Team-7/script.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   generateSubCategoryChart();
+  generateCountryChart();
+  generateCategoryRevenue();
 });
 
 const fetchJson = async () => {
@@ -34,7 +36,7 @@ const generateSubCategoryChart = async () => {
     const labels = dataArray.map((item) => item[0]);
     const values = dataArray.map((item) => item[1]);
 
-    generateChart(
+    generateYChart(
       labels,
       values,
       "Top Selling Products by Sub-Category",
@@ -45,9 +47,72 @@ const generateSubCategoryChart = async () => {
   }
 };
 
-const generateChart = (labels, values, placeholder, chartId) => {
-  const ctx = document.getElementById(chartId).getContext("2d");
+const generateCountryChart = async () => {
+  try {
+    const data = await fetchJson();
 
+    const aggregatedData = data.reduce((acc, curr) => {
+      const country = curr.Country;
+      const orderQuantity = parseFloat(curr.Order_Quantity);
+
+      if (!acc[country] || orderQuantity > acc[country]) {
+        acc[country] = orderQuantity;
+      }
+
+      return acc;
+    }, {});
+
+    const dataArray = Object.entries(aggregatedData);
+    dataArray.sort((a, b) => b[1] - a[1]);
+
+    const labels = dataArray.map((item) => item[0]);
+    const values = dataArray.map((item) => item[1]);
+
+    generateYChart(
+      labels,
+      values,
+      "Top Selling Products by Country",
+      "country"
+    );
+  } catch (error) {
+    alert("Error: " + error);
+  }
+};
+
+const generateCategoryRevenue = async () => {
+  try {
+    const data = await fetchJson();
+
+    const aggregatedData = data.reduce((acc, curr) => {
+      const country = curr.Product_Category;
+      const orderQuantity = parseFloat(curr.Revenue);
+
+      if (!acc[country] || orderQuantity > acc[country]) {
+        acc[country] = orderQuantity;
+      }
+
+      return acc;
+    }, {});
+
+    const dataArray = Object.entries(aggregatedData);
+    dataArray.sort((a, b) => b[1] - a[1]);
+
+    const labels = dataArray.map((item) => item[0]);
+    const values = dataArray.map((item) => item[1]);
+    console.log(labels);
+    generatePieChart(
+      labels,
+      values,
+      "Top Selling Products by Categories (Revenue)",
+      "Category"
+    );
+  } catch (error) {
+    alert("Error: " + error);
+  }
+}
+
+const generateYChart = (labels, values, placeholder, chartId) => {
+  const ctx = document.getElementById(chartId).getContext("2d");
   new Chart(ctx, {
     type: "bar",
     data: {
@@ -65,5 +130,24 @@ const generateChart = (labels, values, placeholder, chartId) => {
     options: {
       indexAxis: "y",
     },
+  });
+};
+
+const generatePieChart = (labels, values, placeholder, chartId) => {
+  new Chart(chartId, {
+    type: "pie",
+    data: {
+      labels: labels,
+      datasets: [{
+        backgroundColor: "rgb(217, 217, 217)",
+        data: values
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: placeholder
+      }
+    }
   });
 };
